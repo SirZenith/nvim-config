2387d0ed000b361f67aa05134d7f1e9ea61a5f63
陆离 <sirzenith@163.com>
2023-07-24 13:30:58 +0800

    refinement: add prefab-cmp prefab loading config

--- lua/user/plugins/SirZenith/prefab-cmp/config.lua
+++ lua/user/plugins/SirZenith/prefab-cmp/config.lua
@@ -4,14 +4,12 @@ local prefab_cmp = require "prefab-cmp"
 user.plugin.prefab_cmp = {
     __new_entry = true,
     flavor = "cocos",
-    path_map_func = function(_)
-        return "not exists"
+    path_map_func = function(path)
+        return path
     end,
+    prefab_loader = {},
 }
 
 return function()
-    prefab_cmp.setup {
-        flavor = user.plugin.prefab_cmp.flavor(),
-        path_map_func = user.plugin.prefab_cmp.path_map_func(),
-    }
+    prefab_cmp.setup(user.plugin.prefab_cmp())
 end
