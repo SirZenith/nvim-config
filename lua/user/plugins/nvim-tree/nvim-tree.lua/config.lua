fc3626584e4c6bede86a4177338e4b86c4da2cf7
陆离 <sirzenith@163.com>
2023-10-07 09:47:54 +0800

    refine: remove deprecated config option

--- lua/user/plugins/nvim-tree/nvim-tree.lua/config.lua
+++ lua/user/plugins/nvim-tree/nvim-tree.lua/config.lua
@@ -93,7 +93,6 @@ user.plugin.nvim_tree = {
     view = {
         width = 30,
         -- height = 30,
-        hide_root_folder = false,
         side = 'left',
         preserve_window_proportions = false,
         number = false,
