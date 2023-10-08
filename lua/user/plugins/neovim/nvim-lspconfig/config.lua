60660d8d2878325b8f2e42dae5ec17153b48c2a5
陆离 <sirzenith@163.com>
2023-10-08 10:22:46 +0800

    refine: complete language server installation description

--- lua/user/plugins/neovim/nvim-lspconfig/config.lua
+++ lua/user/plugins/neovim/nvim-lspconfig/config.lua
@@ -11,8 +11,17 @@ user.plugin.nvim_lspconfig = {
     __new_entry = true,
     config = {},
     lsp_servers = {
-        "bashls",
-        "clangd",
+        {
+            "bashls",
+            install = "npm i -g bash-language-server",
+        },
+        {
+            "clangd",
+            desc = {
+                "check out installation guide at:",
+                "https://clangd.llvm.org/installation.html",
+            },
+        },
         {
             "csharp_ls",
             install = {
@@ -20,25 +29,47 @@ user.plugin.nvim_lspconfig = {
                 "dotnet tool install --global csharp-ls",
             }
         },
-        "cssls",
-        "gopls",
-        "hls",
-        "html",
+        {
+            "cssls",
+            install = "npm i -g vscode-langservers-extracted"
+        },
+        {
+            "gopls",
+            install = "go install golang.org/x/tools/gopls@latest"
+        },
+        {
+            "hls",
+            install = "ghcup install hls",
+            desc = {
+                "installation guide at:",
+                "https://haskell-language-server.readthedocs.io/en/latest/installation.html",
+            },
+        },
+        {
+            "html",
+            install = "npm i -g vscode-langservers-extracted",
+        },
         {
             "jsonls",
             install = "npm i -g vscode-langservers-extracted",
         },
         {
             "pylsp",
-            install = {
-                "pip install python-lsp-server",
-                "pip install pylsp-mypy",
-                "pip install python-lsp-black",
-                "pip install python-lsp-ruff",
-            },
+            install = "pip install 'python-lsp-server[all]'",
         },
-        "rust_analyzer",
-        "lua_ls",
+        {
+            "rust_analyzer",
+            desc = {
+                "installation guide at:",
+                "https://rust-analyzer.github.io/manual.html#installation",
+            }
+        },
+        {
+            "lua_ls",
+            desc = {
+                "repo URL: https://github.com/luals/lua-language-server",
+            }
+        },
         {
             "lemminx",
             desc = {
@@ -51,9 +82,30 @@ user.plugin.nvim_lspconfig = {
             desc = "TypeScript/JavaScript Server",
             install = "npm install -g typescript typescript-language-server",
         },
-        "texlab",
-        "vls",
-        "zls",
+        {
+            "texlab",
+            desc = {
+                "repo URL: https://github.com/latex-lsp/texlab",
+                "recommended to build from source",
+            },
+        },
+        -- "vls",
+        {
+            "v_analyzer",
+            install = {
+                "mkdir temp",
+                "curl 'https://raw.githubusercontent.com/v-analyzer/v-analyzer/master/install.vsh' -O temp/install.vsh",
+                "v -e temp/install.sh",
+                "rm -r temp",
+            }
+        },
+        {
+            "zls",
+            desc = {
+                "installation guide at:",
+                "https://github.com/zigtools/zls#installation",
+            },
+        },
     },
 }
 
