99ccc23add42db2d096ce71893377d69bef6b66b
陆离 <sirzenith@163.com>
2024-01-19 16:33:50 +0800

    refine: update plugins

--- lua/user/plugins/nvim-lualine/lualine.nvim/config.lua
+++ lua/user/plugins/nvim-lualine/lualine.nvim/config.lua
@@ -8,10 +8,10 @@ user.plugin.lualine = {
         component_separators = "|",
         section_separators = { left = "", right = "" },
         disabled_filetypes = {
-            statusline = { "NvimTree" },
+            statusline = {},
             winbar = {},
         },
-        ignore_focus = {},
+        ignore_focus = { "NvimTree" },
         always_divide_middle = true,
         globalstatus = false,
         refresh = {
