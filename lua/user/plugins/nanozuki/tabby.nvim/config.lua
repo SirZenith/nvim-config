6acc31eba0956c17d554a7dea99ffe7fc5c72656
陆离 <sirzenith@163.com>
2024-02-01 04:38:27 +0800

    refine(plugin.tabby): update tabby style

--- lua/user/plugins/nanozuki/tabby.nvim/config.lua
+++ lua/user/plugins/nanozuki/tabby.nvim/config.lua
@@ -45,46 +45,45 @@ return function()
             },
             line.tabs().foreach(function(tab)
                 local is_current = tab.is_current()
-                local hl = is_current and theme.active or theme.inactive
-                -- local sign = is_current and "" or ""
 
-                local win = tab.current_win()
-                local buf = vim.api.nvim_win_get_buf(win.id)
-                local filename = vim.api.nvim_buf_get_name(buf)
-                local extension = filename ~= "" and vim.fn.fnamemodify(filename, ":e") or ""
-                local sign = require "nvim-web-devicons".get_icon(filename, extension, { default = true })
+                local hl = is_current and theme.active or theme.inactive
+
+                local sign = " " .. tab.current_win().file_icon() .. " "
                 local sign_hl = is_current and theme.sign_active or theme.sign
 
-                local tnum = tab.number()
-                local left_sep = tnum == 1 and "" or ""
+                local left_sep = ""
+
+                local right_sep = is_current and "" or ""
 
                 return {
                     line.sep(left_sep, hl, theme.fill),
-                    { sign, hl = sign_hl, margin = " " },
+                    { sign, hl = sign_hl },
                     str_digest(tab.name(), MAX_LEN),
-                    tab.close_btn(""),
-                    line.sep(" ", hl, theme.fill),
+                    tab.close_btn("  "),
+                    line.sep(right_sep, hl, theme.fill),
                     hl = hl,
-                    margin = " ",
                 }
             end),
             line.spacer(),
             line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
                 local is_current = win.is_current()
-                local sign = is_current and "" or ""
+                local sign = is_current and " " or " "
                 local sign_hl = is_current and theme.status_sign_active or theme.status_sign_active
+
+                local left_sep = ""
+                local right_sep = "█"
+
                 return {
-                    line.sep("", theme.status, theme.fill),
-                    { sign, hl = sign_hl, margin = " " },
+                    line.sep(left_sep, theme.status, theme.fill),
+                    { sign, hl = sign_hl, },
                     str_digest(win.buf_name(), MAX_LEN),
-                    line.sep("", theme.status, theme.fill),
+                    line.sep(right_sep, theme.status, theme.fill),
                     hl = theme.status,
-                    margin = " ",
                 }
             end),
             {
                 line.sep("", theme.tail, theme.fill),
-                { "  ", hl = theme.tail },
+                { " 󱂬 ", hl = theme.tail },
             },
             hl = theme.fill,
         }
