cbba03c7e5ca1e775a493f86950719bb5d7d59e7
陆离 <sirzenith@163.com>
2024-01-31 20:43:11 +0800

    refine(plugin): turn off colorizer color code highlight in noice popup

--- lua/user/plugins/norcalli/nvim-colorizer.lua/config.lua
+++ lua/user/plugins/norcalli/nvim-colorizer.lua/config.lua
@@ -1,3 +1,12 @@
+local user = require "user"
+
+user.plugin.colorizer = {
+    __new_entry = true,
+    "*",
+    -- Hilight all filetype except:
+    "!noice",
+}
+
 return function()
-    require "colorizer".setup()
+    require "colorizer".setup(user.plugin.colorizer())
 end
