195e1e1057bfc494aa053cef43b105f71db7caa4
陆离 <sirzenith@163.com>
2024-02-01 01:27:13 +0800

    refine(plugin.tabby): change single quote to double quote

--- lua/user/plugins/nanozuki/tabby.nvim/config.lua
+++ lua/user/plugins/nanozuki/tabby.nvim/config.lua
@@ -40,42 +40,42 @@ return function()
     require "tabby.tabline".set(function(line)
         return {
             {
-                { '  ', hl = theme.head },
-                line.sep(' ', theme.head, theme.fill),
+                { "  ", hl = theme.head },
+                line.sep(" ", theme.head, theme.fill),
             },
             line.tabs().foreach(function(tab)
                 local is_current = tab.is_current()
                 local hl = is_current and theme.active or theme.inactive
-                local sign = is_current and '' or ''
+                local sign = is_current and "" or ""
                 local sign_hl = is_current and theme.sign_active or theme.sign
                 return {
-                    line.sep('', hl, theme.fill),
-                    { sign, hl = sign_hl, margin = ' ' },
+                    line.sep("", hl, theme.fill),
+                    { sign, hl = sign_hl, margin = " " },
                     tab.number(),
                     str_digest(tab.name(), MAX_LEN),
-                    tab.close_btn(''),
-                    line.sep(' ', hl, theme.fill),
+                    tab.close_btn(""),
+                    line.sep(" ", hl, theme.fill),
                     hl = hl,
-                    margin = ' ',
+                    margin = " ",
                 }
             end),
             line.spacer(),
             line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
                 local is_current = win.is_current()
-                local sign = is_current and '' or ''
+                local sign = is_current and "" or ""
                 local sign_hl = is_current and theme.status_sign_active or theme.status_sign_active
                 return {
-                    line.sep('', theme.status, theme.fill),
-                    { sign, hl = sign_hl, margin = ' ' },
+                    line.sep("", theme.status, theme.fill),
+                    { sign, hl = sign_hl, margin = " " },
                     str_digest(win.buf_name(), MAX_LEN),
-                    line.sep(' ', theme.status, theme.fill),
+                    line.sep(" ", theme.status, theme.fill),
                     hl = theme.status,
-                    margin = ' ',
+                    margin = " ",
                 }
             end),
             {
-                line.sep(' ', theme.tail, theme.fill),
-                { '  ', hl = theme.tail },
+                line.sep(" ", theme.tail, theme.fill),
+                { "  ", hl = theme.tail },
             },
             hl = theme.fill,
         }
