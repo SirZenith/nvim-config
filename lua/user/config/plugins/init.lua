a0e3b6b000d3dcec9f80dc0b49fc0eab1d1bd374
陆离 <sirzenith@163.com>
2024-02-16 02:14:21 +0800

    refine(plugin): remove dev mark of oil-vcs-status

--- lua/user/config/plugins/init.lua
+++ lua/user/config/plugins/init.lua
@@ -89,7 +89,7 @@ local specs = {
     {
         -- Display VCS status symbol in oil.nvim buffer's signcolumn
         "SirZenith/oil-vcs-status.nvim",
-        dev = true,
+        -- dev = true,
         dependencies = {
             "stevearc/oil.nvim",
         },
