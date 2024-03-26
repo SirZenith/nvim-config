ce6741eae32bf48959a9d9f889c940f366f84b3a
陆离 <sirzenith@163.com>
2024-03-26 19:11:43 +0800

    refine(snippet): add snippet for gleam

---
+++ lua/user/snippets/lazy-load/gleam.lua
@@ -0,0 +1,19 @@
+local snip_filetype = "gleam"
+local s = require("snippet-loader.utils")
+local makers = s.snippet_makers(snip_filetype)
+-- local sp = makers.sp
+-- local asp = makers.asp
+-- local psp = makers.psp
+-- local apsp = makers.apsp
+
+-- local condsp = makers.condsp
+-- local condasp = makers.condasp
+-- local condpsp = makers.condpsp
+local condapsp = makers.condapsp
+
+-- local regsp = makers.regsp
+-- local regasp = makers.regasp
+-- local regpsp = makers.regpsp
+-- local regapsp = makers.regapsp
+
+condapsp(s.conds.line_begin, "pt ", "|> ")
