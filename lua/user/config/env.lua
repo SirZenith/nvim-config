98739c64bb1478d081a7f362dad73e035ced2875
陆离 <sirzenith@163.com>
2024-09-27 10:51:19 +0800

    refine(env): add default value for CONFIG_HOME when no such variable is found in environment

--- lua/user/config/env.lua
+++ lua/user/config/env.lua
@@ -1,13 +1,20 @@
 local fs_util = require "user.util.fs"
 local log_util = require "user.util.log"
 
+local env_home = vim.env.HOME or vim.env.USERPROFILE
 local env_config_home = vim.env.CONFIG_HOME
+
+if not env_config_home and env_home then
+    env_config_home = fs_util.path_join(env_home, ".config", "shell-environment", "nvim")
+    log_util.warn("no CONFIG_HOME variable found, use default value", env_config_home)
+end
+
 if not env_config_home then
     log_util.error "initialize failed, can't find environment variable 'CONFIG_HOME'"
     return {}
 end
 
-local DEV_PATH = vim.env.DEV_PATH or fs_util.path_join(vim.env.HOME, "Developer")
+local dev_path = vim.env.DEV_PATH or fs_util.path_join(env_home, "Developer")
 
 return {
     ENV_CONFIG_INIT_OK = true,
@@ -17,17 +24,17 @@ return {
 
     PROXY_URL = vim.env.PROXY_URL or "",
     PLATFORM_MARK = vim.env.PLATFORM_MARK or "",
-    HOME = vim.env.HOME or "",
+    HOME = env_home or "",
     APP_PATH = vim.env.APP_PATH or "",
-    DEV_PATH = DEV_PATH,
+    DEV_PATH = dev_path,
     LANG_PATH = vim.env.LANG_PATH or "",
-    PLUGIN_DEV_PATH = fs_util.path_join(DEV_PATH, "nvim-plugins"),
+    PLUGIN_DEV_PATH = fs_util.path_join(dev_path, "nvim-plugins"),
 
     CC = vim.env.CC or "cc",
     FIREFOX_PATH = "",
     PYTHON_PATH = vim.env.PYTHON_PATH or "python",
     TS_LIB_PATH = vim.env.TS_LIB_PATH or "",
-    YARN_GLOBAL_DIR = vim.env.YARN_GLOBAL_DIR or fs_util.path_join(vim.env.HOME or "~", ".config", "yarn", "global"),
+    YARN_GLOBAL_DIR = vim.env.YARN_GLOBAL_DIR or fs_util.path_join(env_home or "~", ".config", "yarn", "global"),
 
     APPDATA = vim.env.APPDATA or "",
 
