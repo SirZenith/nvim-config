f3053cae2592e3480484e65a9651e2a09b01390d
陆离 <sirzenith@163.com>
2025-08-01 15:06:46 +0800

    refine(plugin): delete finalization of nvim-lspconfig, since framework is deprecated

--- lua/user/plugins/nvim-lspconfig/config.lua
+++
@@ -1,8 +0,0 @@
-local user = require "user"
-local lsp_util = require "user.util.lsp"
-
-return function()
-    for name, config in user.lsp.extra_server:pairs() do
-        lsp_util.add_lsp_config(name, config)
-    end
-end
