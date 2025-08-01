b798e67e38d4a5055683729060650f23db766456
陆离 <sirzenith@163.com>
2025-08-01 19:23:54 +0800

    refine: remove deprecated lspconfig framework call

--- lua/user/util/lsp.lua
+++ lua/user/util/lsp.lua
@@ -1,28 +1,4 @@
-local util = require "user.util"
-
-local validate = vim.validate
-local import = util.import
-
 local M = {}
-
--- Add server config to nvim-lspconfig
----@param ls_name string
----@param default_config table<string, any> # `default_config` field for config
----@param extra_opts? table<string, any> # all other fields of config that are optional
-function M.add_lsp_config(ls_name, default_config, extra_opts)
-    local lspconfigs_configs = import "lspconfig.configs"
-    if not lspconfigs_configs then return end
-
-    validate {
-        cmd = { default_config.cmd, { 't', 'f' } },
-        filetypes = { default_config.filetypes, 't' },
-        root_dir = { default_config.root_dir, 'f' },
-    }
-
-    local config = vim.tbl_deep_extend("force", {}, extra_opts or {})
-    config.default_config = default_config
-    lspconfigs_configs[ls_name] = config
-end
 
 ---@param config table
 ---@param dot_path string
