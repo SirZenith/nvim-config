a684b2bf1431fd9943a3fc769bd70e45cc88ad07
陆离 <sirzenith@163.com>
2025-08-05 11:46:32 +0800

    refine(lsp): remove usage of lsp config util

--- lua/user/config/lsp/configs/markdown.lua
+++ lua/user/config/lsp/configs/markdown.lua
@@ -1,13 +1,13 @@
-return {
+---@type vim.lsp.Config
+local M = {
     cmd = { "vscode-markdown-language-server", "--stdio" },
     filetypes = { "markdown" },
-    root_dir = function()
-        local lspconfig_util = require "lspconfig.util"
-        return lspconfig_util.root_pattern(".git")()
-    end,
+    root_markers = { ".git" },
     single_file_support = true,
     settings = {},
     init_options = {
         markdownFileExtensions = { "md" },
     },
 }
+
+return M
