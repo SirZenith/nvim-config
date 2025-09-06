ad06cdc1ae0feaf8d4b6b63580ca97e61351ff11
陆离 <sirzenith@163.com>
2025-09-07 00:04:21 +0800

    refine(snippet.delite): improve init snippet

--- lua/user/config/snippet/configs/conditional-load/delite-snippet.lua
+++ lua/user/config/snippet/configs/conditional-load/delite-snippet.lua
@@ -45,14 +45,6 @@ function M.setup()
                 { "source-dir", is_optional = true },
             },
             content = function(source_dir_arg)
-                if not source_dir_arg then
-                    return SCRIPT_INIT
-                end
-
-                local file_name = vim.api.nvim_buf_get_name(0)
-                local source_dir = vim.fs.joinpath(vim.fs.dirname(file_name), source_dir_arg)
-                local files = fs_util.listdir(source_dir)
-
                 local head = {
                     "local delite = require \"delite\"",
                     "local html = require \"html\"",
@@ -61,26 +53,34 @@ function M.setup()
                     "local Node = html.Node",
                     "local NodeType = html.NodeType",
                     "",
-
                     "---@param toc_filename string",
-                    "local function setup_chapter_title(toc_filename)",
+                    "local function default_chapter_handler(toc_filename)",
                     "end",
                     "",
                     "---@class CommonSetupArgs",
                     "---@field delete_files string[]",
                     "---@field toc_filename string",
+                    "---@field chapter_title_handler? fun(toc_filename: string)",
                     "---@field pagebreak_before string[]",
                     "",
                     "---@param args CommonSetupArgs",
                     "local function common_handler(args)",
-                    "    delite.delete_file_content(doc_node, args.delete_files)",
+                    "    local toc_filename = args.toc_filename",
+                    '    if toc_filename ~= "" then',
+                    '        if type(args.chapter_title_handler) == "function" then',
+                    "            args.chapter_title_handler(toc_filename)",
+                    "        else",
+                    "            default_chapter_handler(args.toc_filename)",
+                    "        end",
                     "",
-                    "    setup_chapter_title(args.toc_filename)",
-                    "    delite.replace_file_content_with_toc(doc_node, args.toc_filename)",
+                    "        delite.replace_file_content_with_toc(doc_node, args.toc_filename)",
+                    "    end",
                     "",
                     "    for _, file in ipairs(args.pagebreak_before) do",
                     "        delite.add_pagebreak_before_file(doc_node, file)",
                     "    end",
+                    "",
+                    "    delite.delete_file_content(doc_node, args.delete_files)",
                     "end",
                     "",
                     "delite.switch_handler(meta.source_filename, {",
@@ -92,11 +92,19 @@ function M.setup()
                     "",
                     "return doc_node",
                 }
-
                 local entries = {}
-                for _, file in ipairs(files) do
-                    local basename = vim.fs.basename(file)
-                    table.insert(entries, ("    [\"%s\"] = fnil,"):format(basename))
+
+                if source_dir_arg then
+                    local file_name = vim.api.nvim_buf_get_name(0)
+                    local source_dir = vim.fs.joinpath(vim.fs.dirname(file_name), source_dir_arg)
+                    local files = fs_util.listdir(source_dir)
+
+                    for _, file in ipairs(files) do
+                        local basename = vim.fs.basename(file)
+                        table.insert(entries, ("    [\"%s\"] = fnil,"):format(basename))
+                    end
+                else
+                    table.insert(entries, "    _ = fnil,")
                 end
 
                 local result = {}
@@ -106,7 +114,17 @@ function M.setup()
 
                 return result
             end,
-        }
+        },
+
+        ["new common-handling"] = {
+            content = {
+                "common_handler {",
+                "    delete_files = {},",
+                '    toc_filename = "",',
+                "    pagebreak_before = {},",
+                "}",
+            }
+        },
     })
 end
 
