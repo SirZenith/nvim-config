77e10ae16b21e4d27667f3b97befb9562a3bea2b
陆离 <sirzenith@163.com>
2026-03-20 13:12:56 +0800

    fix(plugin.leap.nvim): fix keymapping

--- lua/user/config/plugin/configs/leap.nvim/config.lua
+++ lua/user/config/plugin/configs/leap.nvim/config.lua
@@ -1,5 +1,14 @@
 return function()
     local leap = require "leap"
 
-    leap.add_default_mappings()
+    for _, t in ipairs {
+        { { "n", "x", "o" }, "s",  "<Plug>(leap-forward)",  "Leap forward to" },
+        { { "n", "x", "o" }, "S",  "<Plug>(leap-backward)", "Leap backward to" },
+        { { "n", "x", "o" }, "js", "<Plug>(leap-anywhere)", "Leap anywhere" },
+    } do
+        local modes, lhs, rhs, desc = unpack(t)
+        for _, mode in ipairs(modes) do
+            vim.keymap.set(mode, lhs, rhs, { silent = true, desc = desc })
+        end
+    end
 end
